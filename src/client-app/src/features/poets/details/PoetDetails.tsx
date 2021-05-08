dda75ab324004837c28f4bfc18b9d898b66550ac
hossein <hossein.a365@gmail.com>
2021-05-09 02:11:54 +0430

    modal

--- src/client-app/src/features/poets/details/PoetDetails.tsx
+++ src/client-app/src/features/poets/details/PoetDetails.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Image, Button } from 'semantic-ui-react';
+import { Card, Image, Button,Modal, Header } from 'semantic-ui-react';
 import { IPoet } from '../../../app/models/poet';
 
 interface IProps {
@@ -8,24 +8,52 @@ interface IProps {
 }
 
 const PoetDetails: React.FC<IProps> = ({poet, setSelectedPoet}) => {
+  const [open, setOpen] = React.useState(true)
+
   return (
-    <Card fluid>
-      <Image src={`/assets/poet/${poet.url}.gif`} wrapped ui={false} />
-      <Card.Content>
-        <Card.Header>{poet.name}</Card.Header>
-        <Card.Meta>
-          <span>{poet.url}</span>
-        </Card.Meta>
-        <Card.Description>
-          {poet.description}
-        </Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <Button.Group widths={2}>
-             <Button onClick={() => setSelectedPoet(null)} basic color='grey' content='Cancel' />
-        </Button.Group>
-      </Card.Content>
-    </Card>
+    <Modal
+    onClose={() =>{ setOpen(false);setSelectedPoet(null)}}
+    onOpen={() => setOpen(true)}
+    open={open}
+   >
+    <Modal.Header>{poet.name}</Modal.Header>
+    <Modal.Content image>
+      <Image size='large' src={`/assets/poet/${poet.url}.gif`} wrapped />
+      <Modal.Description>
+        <Header>{poet.name}</Header>
+        <p>
+        {poet.description}
+        </p>
+      </Modal.Description>
+    </Modal.Content>
+    <Modal.Actions>
+
+      <Button
+        content="باشه دیگه "
+        labelPosition='right'
+        icon='checkmark'
+        onClick={() => {setOpen(false);setSelectedPoet(null)}}
+        positive
+      />
+    </Modal.Actions>
+  </Modal>
+
+    // <Card fluid>
+    //   <Card.Content>
+    //     <Card.Header>{poet.name}</Card.Header>
+    //     <Card.Meta>
+    //       <span>{poet.url}</span>
+    //     </Card.Meta>
+    //     <Card.Description>
+    //       {poet.description}
+    //     </Card.Description>
+    //   </Card.Content>
+    //   <Card.Content extra>
+    //     <Button.Group widths={2}>
+    //          <Button onClick={() => setSelectedPoet(null)} basic color='grey' content='Cancel' />
+    //     </Button.Group>
+    //   </Card.Content>
+    // </Card>
   );
 };
 
