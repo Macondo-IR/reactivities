1a9f3e85238f0d74af033416c8f6288f056f2216
hossein <hossein.a365@gmail.com>
2021-05-14 11:51:05 +0430

    final prepare for adding AccountController

---
+++ src/SecondApi/DTOs/LoginDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecondApi.DTOs
+{
+    public class LoginDto
+    {
+        public string Email { get; set; }
+        public string Paswword { get; set; }
+    }
+}
