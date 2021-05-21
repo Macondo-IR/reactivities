6b85509a622e69e0f344281348bbfd7d5957b98d
hossein <hossein.a365@gmail.com>
2021-05-21 22:47:16 +0430

    npm i @types/yup --save

--- src/client/src/features/activities/form/ActivityForm.tsx
+++ src/client/src/features/activities/form/ActivityForm.tsx
@@ -1,12 +1,12 @@
 import { useState, ChangeEvent, useEffect } from 'react';
-import { Segment, Form, Button } from 'semantic-ui-react';
+import { Segment, Button } from 'semantic-ui-react';
 import { useStore } from '../../../app/stores/store';
 import { observer } from 'mobx-react-lite';
 import { useHistory, useParams } from 'react-router';
 import LoadingComponents from '../../../app/layout/LoadingComponents';
 import {v4 as uuid } from 'uuid'; 
 import { Link } from 'react-router-dom';
-import { Formik } from 'formik';
+import { Formik,Form,Field } from 'formik';
 
 export default observer(function ActivityForm(){
   const history=useHistory();
@@ -43,7 +43,7 @@ export default observer(function ActivityForm(){
   //     updateActivity(activity).then(()=>history.push(`/activities/${activity.id}`))
   //   }
   // }
-  // function handleInputChange (event:ChangeEvent<HTMLInputElement | HTMLTextAreaElement>)
+  // function handleChange (event:ChangeEvent<HTMLInputElement | HTMLTextAreaElement>)
   // {
   //   const { name, value } = event.target; 
   //   setActivity({ ...activity, [name]: value })
@@ -51,18 +51,18 @@ export default observer(function ActivityForm(){
   if(loadingInitial) return <LoadingComponents content='Loading' />
   return (
     <Segment clearing>
-      <Formik initialValues={activity} onSubmit={values=>console.log(values)}>
-        {({values,handleChange,handleSubmit})=>(
-             <Form onSubmit={handleSubmit} autoComplete='off'>
-             <Form.Input placeholder='Title' value={activity.title} name='title' onChange={handleInputChange} />
-             <Form.TextArea placeholder='Description' value={activity.description} name='description' onChange={handleInputChange} />
-             <Form.Input placeholder='Category' value={activity.category} name='category' onChange={handleInputChange} />
-             <Form.Input type='datetime-local' placeholder='Date' value={activity.date} name='date' onChange={handleInputChange} />
-             <Form.Input  placeholder='City' value={activity.city} name='city' onChange={handleInputChange} />
-             <Form.Input  placeholder='Venue' value={activity.venue} name='venue' onChange={handleInputChange} />
-             <Button  loading={loading} floated='right' positive type='submit' content='Submit' />
-             <Button  as={Link} to='/activities'   floated='right' type='button' content='Cancel' />
-           </Form>
+      <Formik enableReinitialize initialValues={activity} onSubmit={values=>console.log(values)}>
+        {({handleSubmit})=>(
+          <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
+            <Field placeholder='Title' name='title'  />
+            <Field placeholder='Description'  name='description'  />
+            <Field placeholder='Category'  name='category'  />
+            <Field type='datetime-local' placeholder='Date'  name='date'  />
+            <Field  placeholder='City'  name='city'  />
+            <Field  placeholder='Venue'  name='venue'  />
+            <Button  loading={loading} floated='right' positive type='submit' content='Submit' />
+            <Button  as={Link} to='/activities'   floated='right' type='button' content='Cancel' />
+          </Form>
         )}
       </Formik>
    
